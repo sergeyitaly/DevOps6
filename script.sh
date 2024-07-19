3e04d92f1b3b3e577f599f9ea1623dba831e94ca
sergeyitaly <96667927+sergeyitaly@users.noreply.github.com>
2024-07-19 14:14:37 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -7,6 +7,9 @@ current_date=$(date +"%Y-%m-%d")
 mkdir -p ~/linux_p2/backup
 mkdir -p ~/linux_p2/old_backup
 
+# Перевірка і переміщення старих архівів
+find ~/linux_p2/backup -type f -name "*.tar.gz" -mmin +3 -exec mv {} ~/linux_p2/old_backup/ \;
+
 # Прохід по всіх файлах у директорії ~/linux_p2
 for file in ~/linux_p2/*.txt; do
     # Отримання назви файлу без шляху
@@ -18,7 +21,3 @@ for file in ~/linux_p2/*.txt; do
     # Архівування файлу
     tar -czf ~/linux_p2/backup/"${filename}_${current_date}.tar.gz" -C ~/linux_p2 "$filename"
 done
-
-# Перевірка і переміщення старих архівів
-find ~/linux_p2/backup -type f -name "*.tar.gz" -mmin +3 -exec mv {} ~/linux_p2/old_backup/ \;
-#!/bin/bash
